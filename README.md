926228d6b82037c654976b8869f2d64abd0b78cd
kiran ps <pskirann@gmail.com>
2015-03-09 15:41:00 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+### Beagle board getting started app using nodewebkit, angular
+
+to run this app download nodewebkit and run the following command from project directory
+
+    nodewebkit .
+
+or
+
+    nw .
---
+++ scripts/app.js
@@ -0,0 +1,39 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name yeomanApp
+ * @description
+ * # yeomanApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('yeomanApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl'
+      })
+      .when('/devices', {
+        templateUrl: 'views/devices.html',
+      })
+      .when('/updates', {
+        templateUrl: 'views/updates.html',
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
